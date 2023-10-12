f19e7054e88ecbbba0818cce0ae2b8e050924709
Natalie <favoritewillow@gmail.com>
2023-10-12 15:59:30 +0300

    feat: add extractNameFromTemplate (Thursday 12.10.2023 15:59am)

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -65,8 +65,12 @@ function getStringFromTemplate(firstName, lastName) {
  *   'Hello, John Doe!' => 'John Doe'
  *   'Hello, Chuck Norris!' => 'Chuck Norris'
  */
-function extractNameFromTemplate(/* value */) {
-  throw new Error('Not implemented');
+function extractNameFromTemplate(value) {
+  const words = value.split(' ');  
+  const firstName = words[1];
+  const lastName = words[2];  
+  const fullName = `${firstName} ${lastName}`;
+  return fullName;
 }
 
 
